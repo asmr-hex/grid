c8966398e00b4b6cd25f184b4390310cba454116
connorwalsh <c@polygon.pizza>
2019-11-21 20:52:36 -0500

    define waveform type

--- src/animation/types.hpp
+++ src/animation/types.hpp
@@ -1,6 +1,30 @@
 #ifndef ANIMATION_TYPES_H
 #define ANIMATION_TYPES_H
 
+#include <map>
+
+
+enum WaveformTypes {Sine, Saw, Unit};
+
 // TODO define animation types!
+struct waveform {
+public:
+  int amplitude;
+  struct {
+    float frequency;
+    WaveformTypes type;
+  } modulator;
+  struct {
+    float duty_cycle;
+    float frequency;
+  } step_fn;
+
+  int compute(int time_step) {
+    return 0;
+  };
+  
+private:
+  std::map<float, int> cache;
+};
 
 #endif
