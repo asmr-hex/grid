d991605e6aa466715d6f64bf67b32c40c07da8da
connorwalsh <c@polygon.pizza>
2020-01-17 12:38:49 -0500

    begin implementing sequences state

--- src/anemone/state/high_frequency/sequences.hpp
+++ src/anemone/state/high_frequency/sequences.hpp
@@ -9,9 +9,31 @@
 #ifndef STATE_HIGH_FREQUENCY_SEQUENCES_H
 #define STATE_HIGH_FREQUENCY_SEQUENCES_H
 
+#include "anemone/types.hpp"
+
 
 namespace State {
 
+  /// @brief state sequence type containing various types of midi sequences.
+  struct sequence_t {
+    types::sequence_t midi_on;
+    types::sequence_t midi_off;
+    types::sequence_t midi_cc;
+    types::sequence_t midi_nrpn;
+  };
+
+  /// @brief mapping of instrument to part to sequences.
+  typedef std::map<InstrumentName, std::map<unsigned int, sequence_t> > sequences_t;
+
+  /// @brief ...
+  class Sequences : public rx::Observable<std::shared_ptr<sequences_t> > {
+  public:
+    rx::types::high_frequency::state_ptr<sequences_t, high_freq_action_t> state;
+
+    Sequences();
+
+    virtual std::shared_ptr<rx::dag::Observable<std::shared_ptr<sequences_t> > > get() override;
+  };
   
 }
 
---
+++ src/anemone/types.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file   types.hpp
+ * @brief  Includes all necessary headers from types
+ * @author coco
+ * @date   2020-01-17
+ *************************************************/
+
+
+#ifndef ANEMONE_TYPES_H
+#define ANEMONE_TYPES_H
+
+
+#include "anemone/types/sequencer.hpp"
+
+
+#endif
