167f466cecc91ae4316c9f6eb50c0a972d022187
asmr-hex <0.0@asmr.software>
2021-07-22 12:33:16 -0400

    experimenting with generated c++ from xsd

---
+++ conf-experiment/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "conf/schema.hpp"
+
+int main (int argc, char* argv[]) {
+    try {
+        std::unique_ptr<Interface> i = Interface_("../conf-xml/test.xml");
+
+        std::printf("INTERFACE: %s\n", i->name());
+
+        // for (auto output : i->Outputs) {
+        //     std::printf("OUTPUT: %s", output->name);
+        // }
+    }
+    catch (const xml_schema::exception& e) {
+        std::cerr << e << std::endl;
+        return 1;
+    }
+
+}
