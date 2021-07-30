1502051c09c179b63f3a5ebed9403bc6c7156788
asmr-hex <0.0@asmr.software>
2021-07-30 16:59:23 -0400

    refactor Sequencer api

--- src/modules/sequencer.cpp
+++ src/modules/sequencer.cpp
@@ -4,67 +4,58 @@
 
 using namespace opr::module;
 
-// Sequencer::Sequencer()
-//   : step({ .current = rxcpp::subjects::behavior<step_idx_t>(0) })
-// {}
+Sequencer::Sequencer() : clock(*this) {}
 
-void Sequencer::connect() {
-    std::printf("sequencer loadingsx\n");
+Sequencer::Clock::Clock(Sequencer& s) : sequencer(s) {}
+
+void Sequencer::Clock::sync(std::string name, rx::observable<bool> clk) {
+    // add new clock to clocks mapping
+    clocks[name] = clk;
+
+    // merge clock streams
+    observable = clocks.size() == 1
+        ? clk
+        : static_cast<rx::observable<bool>>(observable.merge(clk));
+
+    subscribe();
 }
 
-void Sequencer::sync(std::string name, rx::observable<bool> clk) {
-  // add new clock to clocks mapping
-  clocks[name] = clk;
+void Sequencer::Clock::unsync(std::string name) {
+    // remove clock from mapping
+    clocks.erase(name);
 
-  // merge new clock observable
-  if (clocks.size() == 1) {
-    merged_clocks_observable = clk;
-  } else {
-    merged_clocks_observable = merged_clocks_observable.merge(clk);     
-  }
-
-  // subscribe
-  subscribe_to_clocks();
-}
-
-void Sequencer::subscribe_to_clocks() {
-  // unsubscribe
-  clocks_subscription.unsubscribe();
-
-  clocks_subscription = merged_clocks_observable
-    .subscribe([this] (bool _) {
-                 step.current(step.current() + step.stride.get_value());
-               });
-}
-
-void Sequencer::unsync(std::string name) {
-  // remove clock from mapping
-  clocks.erase(name);
-
-  if (clocks.size() == 0) return;
-  
-  // create new merged subscription
-  rx::observable<bool> o;
-  bool init = true;
-  for (auto itr : clocks) {
-    if (init) {
-      o = itr.second;
-      init = false;
-    } else {
-      o = o.merge(itr.second); 
+    // unsubscribe & return early if there are no more clocks
+    if (clocks.size() == 0 ) {
+        subscription.unsubscribe();
+        return;
     }
-  }
-  merged_clocks_observable = o;
-  
-  subscribe_to_clocks();
+
+    // create new merged subscription
+    bool init = false;
+    for (auto itr : clocks) {
+      if (!init) {
+          observable = itr.second;
+          init = true;
+          continue;
+      }
+      observable = observable.merge(itr.second);
+    }
+
+    // re-subscribe to clocks
+    subscribe();
 }
 
+void Sequencer::Clock::subscribe() {
+  // always unsubscribe before subscribing (TODO: edge case when we haven't subscribed yet)
+  subscription.unsubscribe();
 
-void Sequencer::Step::current(step_idx_t i) {
-  current_m.get_subscriber().on_next(i);
+  // subscribe to clock ticks
+  subscription = observable
+    .subscribe([this] (bool _) {
+      auto current = sequencer.step.current.get_value();
+      auto stride  = sequencer.step.stride.get_value();
+      sequencer.step.current
+        .get_subscriber()
+        .on_next(current + stride);
+    });
 }
-
-step_idx_t Sequencer::Step::current() {
-  return current_m.get_value();
-}
-
--- tests/unit/modules/test_sequencer.cpp
+++ tests/unit/modules/test_sequencer.cpp
@@ -7,14 +7,14 @@ SCENARIO( "Sequencer driven by a single clock" ) {
   GIVEN( "a single clock and a Sequencer" ) {
     opr::module::Sequencer* s = new opr::module::Sequencer();
     rx::behavior<bool> clk = rx::behavior<bool>(true);
-    s->sync("A", clk.get_observable().skip(1));
-    
+    s->clock.sync("A", clk.get_observable().skip(1));
+
     WHEN( "the clock ticks" ) {
       clk.get_subscriber().on_next(false);
       THEN( "the Sequencer advances" ) {
-        REQUIRE( s->step.current() == 1 );
+        REQUIRE( s->step.current.get_value() == 1 );
       }
-    }    
+    }
   }
 }
 
@@ -23,43 +23,43 @@ SCENARIO( "Sequencer driven by multiple clocks" ) {
     opr::module::Sequencer* s = new opr::module::Sequencer();
     rx::behavior<bool> clkA = rx::behavior<bool>(true);
     rx::behavior<bool> clkB = rx::behavior<bool>(true);
-    s->sync("A", clkA.get_observable().skip(1));
-    s->sync("B", clkB.get_observable().skip(1));
-    
+    s->clock.sync("A", clkA.get_observable().skip(1));
+    s->clock.sync("B", clkB.get_observable().skip(1));
+
     WHEN( "clock only A ticks" ) {
       clkA.get_subscriber().on_next(true);
       THEN( "the Sequencer advances" ) {
-        REQUIRE( s->step.current() == 1 );
+        REQUIRE( s->step.current.get_value() == 1 );
       }
     }
 
     WHEN( "clock only B ticks" ) {
       clkB.get_subscriber().on_next(true);
       THEN( "the Sequencer advances" ) {
-        REQUIRE( s->step.current() == 1 );
+        REQUIRE( s->step.current.get_value() == 1 );
       }
     }
 
     WHEN( "clock B & A tick" ) {
       clkA.get_subscriber().on_next(true);
-      REQUIRE( s->step.current() == 1 );
+      REQUIRE( s->step.current.get_value() == 1 );
       clkB.get_subscriber().on_next(true);
       THEN( "the Sequencer advances" ) {
-        REQUIRE( s->step.current() == 2 );
+        REQUIRE( s->step.current.get_value() == 2 );
       }
     }
 
     WHEN( "clock A ticks, clock B ticks, clock B unsyncs, clock B ticks" ) {
       clkA.get_subscriber().on_next(true);
-      REQUIRE( s->step.current() == 1 );
+      REQUIRE( s->step.current.get_value() == 1 );
       clkB.get_subscriber().on_next(true);
-      REQUIRE( s->step.current() == 2 );
-      s->unsync("B");
+      REQUIRE( s->step.current.get_value() == 2 );
+      s->clock.unsync("B");
       clkB.get_subscriber().on_next(true);
-      REQUIRE( s->step.current() == 2 );
+      REQUIRE( s->step.current.get_value() == 2 );
       clkA.get_subscriber().on_next(true);
       THEN( "the Sequencer advances" ) {
-        REQUIRE( s->step.current() == 3 );
+        REQUIRE( s->step.current.get_value() == 3 );
       }
     }
   }
